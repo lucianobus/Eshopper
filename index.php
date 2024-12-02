ba7ddfc029c414ed369ac69193cb6c87148f0113
lucianobustamante <lb1550904@gmail.com>
2024-12-02 14:11:09 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="Software Punto de Venta" content="">
-    <meta name="Analy Reyes " content="">
+    <meta name="Luciano " content="">
     <title>Inicio | E-Shopper</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
